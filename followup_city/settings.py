5f61c55d94aa92502149f7a2e10f7812852be7eb
Bayo <sitanetglobaltech@gmail.com>
2023-06-13 13:11:13 +0100

    about to migrate to aws

--- followup_city/settings.py
+++ followup_city/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['django-followup-env.eba-jctnmhyp.us-west-2.elasticbeanstalk.com ']
+ALLOWED_HOSTS = []
 
 
 # Application definition
