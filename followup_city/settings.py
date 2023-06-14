62a99c2d2176c60825144a910a20bd5954fed609
Bayo <sitanetglobaltech@gmail.com>
2023-06-14 14:33:28 +0100

    decouple

--- followup_city/settings.py
+++ followup_city/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# # SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY=django-insecure-o2hc%d(uxxqc5r*4t4!!qwp&39(f)^eh5!*uwy&xxirics1xak
-SECRET_KEY =django-insecure-o2hc%d(uxxqc5r*4t4!!qwp&39(f)^eh5!*uwy&xxirics1xak
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
