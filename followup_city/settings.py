000df8e84b2156bee95f44f82268f2c8d5f342f1
Bayo <sitanetglobaltech@gmail.com>
2023-06-14 14:20:39 +0100

    import config4

--- followup_city/settings.py
+++ followup_city/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # # SECRET_KEY = config('SECRET_KEY')
 # SECRET_KEY=django-insecure-o2hc%d(uxxqc5r*4t4!!qwp&39(f)^eh5!*uwy&xxirics1xak
-SECRET_KEY = 'django-insecure-o2hc%d(uxxqc5r*4t4!!qwp&39(f)^eh5!*uwy&xxirics1xak'
+# SECRET_KEY = 'django-insecure-o2hc%d(uxxqc5r*4t4!!qwp&39(f)^eh5!*uwy&xxirics1xak'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
