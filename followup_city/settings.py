7afecbae5bfdf19fc609855c2038a4937f60b392
Bayo <sitanetglobaltech@gmail.com>
2023-06-14 14:44:52 +0100

    env2

--- followup_city/settings.py
+++ followup_city/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-o2hc%d(uxxqc5r*4t4!!qwp&39(f)^eh5!*uwy&xxirics1xak')
+SECRET_KEY = config('SECRET_KEY', default='mydefaultkey')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
