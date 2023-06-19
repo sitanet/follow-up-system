03ba683c91a27a3fac0f68d2717e068dd75a674b
Bayo <sitanetglobaltech@gmail.com>
2023-06-19 19:37:31 +0100

    push

--- followup_city/urls.py
+++ followup_city/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     
     
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
